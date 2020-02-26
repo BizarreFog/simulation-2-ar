970ee7adfadb290db2e6619e9e6ba7b7f8055a06
TomasAbrams <TSAbrams3039@muleriders.saumag.edu>
2020-02-25 20:31:41 -0600

    GPS changes and goodbye map box

--- Assets/Scripts/GPSPrototype.cs
+++ Assets/Scripts/GPSPrototype.cs
@@ -6,28 +6,40 @@ using UnityEngine.UI;
 public class GPSPrototype : MonoBehaviour
 {
     public Text text;
+    public LocationServiceStatus service;
     public bool locationEnabled;
 
     private void Start()
     {
-        StartCoroutine("BeginLocationChecking();");
+        StartCoroutine(BeginLocationChecking());
     }
+
+    private void Update()
+    {
+        service = Input.location.status;
+    }
+
+
 
     IEnumerator BeginLocationChecking()
     {
+        yield return new WaitForSeconds(3f);
+
         text.text = "Getting Data...";
 
         if (!Input.location.isEnabledByUser)
         {
+            print("Location data not enabled.");
             yield break;
         }
 
         Input.location.Start();
 
-        int maxWait = 20;
+        int maxWait = 10;
 
-        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        while (Input.location.status != LocationServiceStatus.Running && maxWait > 0)
         {
+            print ("Location data not enabled.");
             yield return new WaitForSeconds(1);
             maxWait--;
         }
@@ -36,20 +48,25 @@ public class GPSPrototype : MonoBehaviour
         if (maxWait < 1)
         {
             //print("Timed out");
-            text.text = "Timed Out";
+            print("Timed Out");
             yield break;
         }
 
         // Connection has failed
         if (Input.location.status == LocationServiceStatus.Failed)
         {
-            text.text = "Unable to determine device location";
+            print("Unable to determine device location");
             yield break;
         }
         else
         {
-            // Access granted and location value could be retrieved
-            text.text = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
+            while (Input.location.status == LocationServiceStatus.Running)
+            {
+                print("Update");
+                Input.location.Start();
+                text.text = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
+                yield return new WaitForSecondsRealtime(.5f);
+            }
         }
 
         // Stop service if there is no need to query location updates continuously
