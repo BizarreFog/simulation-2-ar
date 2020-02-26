48142a6834242a91410e9dd0d4d239714abbb430
ShaneBarner <sjbarner@hotmail.com>
2020-02-25 20:28:17 -0600

    Ui work

---
+++ Assets/Scripts/Shane/exit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exit : MonoBehaviour
+{
+    public void doexit()
+    {
+        Debug.Log("has exited");
+        Application.Quit();
+    }
+}
